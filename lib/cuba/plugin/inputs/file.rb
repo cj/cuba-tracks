d85a77cbfde4aaf311344a371a39a157f0b916cb
cj <cjlazell@gmail.com>
2014-03-25 15:36:18 -0500

    updates

--- lib/cuba/plugin/inputs/file.rb
+++ lib/cuba/plugin/inputs/file.rb
@@ -4,12 +4,11 @@ module FormBuilder
       key = options[:s3_upload_path].call(record)
 
       mab do
-        if options[:value]
-          input id: id, type: :hidden, name: options[:name], class: 'form-control file', value: options[:value], 'data-s3-uploader' => S3Uploader.js_button_options(id, key, options[:success_url], options[:params])
-        else
-          div id: id, name: options[:name], class: 'form-control file', value: options[:value], 'data-s3-uploader' => S3Uploader.js_button_options(id, key, options[:success_url], options[:params])
-          text! S3Uploader.js_button(id, key, options[:success_url], options[:params])
+        unless options[:value]
+          div id: id, name: options[:name], class: 'form-control file s3-uploader', value: options[:value]
         end
+        input id: id, type: :hidden, name: options[:name], class: 'form-control file s3-uploader', value: options[:value]
+        text! S3Uploader.js_button(id, key, options[:callback_url], options[:callback_params])
       end
     end
   end
