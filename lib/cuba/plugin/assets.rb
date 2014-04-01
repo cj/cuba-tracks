47c2bdfb69d379be486014a89faf7f0b8f795895
cj <cjlazell@gmail.com>
2014-04-01 12:19:03 -0500

    combine assets in production

--- lib/cuba/plugin/assets.rb
+++ lib/cuba/plugin/assets.rb
@@ -79,6 +79,60 @@ module Assets
     Assets.app.settings[:assets]
   end
 
+  def javascript_links
+    assets = []
+
+    js_assets.each do |asset|
+      assets.push asset.gsub(/coffee/, 'js')
+    end
+
+    mab do
+      if Cuba.env.production? or Cuba.env.staging?
+        script 'data-turbolinks-track' => true, src: asset_path(assets.join(','))
+      else
+        assets.each do |asset|
+          script 'data-turbolinks-track' => true, src: asset_path(asset)
+        end
+      end
+    end
+  end
+
+  %w(css js js_head).each do |type|
+    define_method "#{type}_links" do
+      assets = []
+
+      send("#{type}_assets").each do |asset|
+        assets.push asset.gsub(/\.coffee/, '.js').gsub(/\.scss/, '.css')
+      end
+
+      return_links type, assets
+    end
+  end
+
+  private
+
+  def return_links type, assets
+    mab do
+      if Cuba.env.production? or Cuba.env.staging?
+        if type.to_s == 'js' or type.to_s == 'js_head'
+          script 'data-turbolinks-track' => 'true', src: asset_path(assets.join(','))
+        else
+          link 'data-turbolinks-track' => 'true', href: asset_path(assets.join(',')),
+            rel: 'stylesheet', type: 'text/css', media: 'all'
+        end
+      else
+        assets.each do |asset|
+          if type.to_s == 'js' or type.to_s == 'js_head'
+            script 'data-turbolinks-track' => true, src: asset_path(asset)
+          else
+            link 'data-turbolinks-track' => 'true', href: asset_path(asset),
+              rel: 'stylesheet', type: 'text/css', media: 'all'
+          end
+        end
+      end
+    end
+  end
+
   module ClassMethods
     %w(css image js js_head).each do |type|
       define_method "#{type}_assets" do |files|
@@ -108,35 +162,58 @@ module Assets
   end
 
   class App < Cuba
-    define do
-      on get, accepted_assets do |file, ext|
-        res.headers["Content-Type"] = "#{MimeMagic.by_extension(ext).to_s}; charset=utf-8"
-
-        if file[/bower/]
-          if ext == 'css' and css_assets.include? file + '.scss'
-            res.write render "#{Assets.app.root}/app/assets/#{file}.scss"
-          elsif js_assets.include? file + '.coffee' or js_head_assets.include? file + '.coffee'
-            res.write render "#{Assets.app.root}/app/assets/#{file}.coffee"
+    def add_asset file, ext
+      if file[/bower/]
+        if ext == 'css' and css_assets.include? file + '.scss'
+          render "#{Assets.app.root}/app/assets/#{file}.scss"
+        elsif js_assets.include? file + '.coffee' or js_head_assets.include? file + '.coffee'
+          render "#{Assets.app.root}/app/assets/#{file}.coffee"
+        else
+          File.read "#{Assets.app.root}/app/assets/#{file}.#{ext}"
+        end
+      else
+        case ext
+        when 'css'
+          if css_assets.include? file + '.scss'
+            render "#{Assets.app.root}/app/assets/stylesheets/#{file}.scss"
           else
-            res.write File.read "#{Assets.app.root}/app/assets/#{file}.#{ext}"
+            File.read "#{Assets.app.root}/app/assets/stylesheets/#{file}.#{ext}"
+          end
+        when 'js'
+          if js_assets.include? file + '.coffee'or js_head_assets.include? file + '.coffee'
+            render "#{Assets.app.root}/app/assets/javascripts/#{file}.coffee"
+          else
+            File.read "#{Assets.app.root}/app/assets/javascripts/#{file}.#{ext}"
           end
         else
-          case ext
-          when 'css'
-            if css_assets.include? file + '.scss'
-              res.write render "#{Assets.app.root}/app/assets/stylesheets/#{file}.scss"
-            else
-              res.write File.read "#{Assets.app.root}/app/assets/stylesheets/#{file}.#{ext}"
+          File.read "#{Assets.app.root}/app/assets/#{file}.#{ext}"
+        end
+      end
+    end
+
+    define do
+      on get, accepted_assets do |file, ext|
+        if file[/,/]
+          file_content = ''
+          file += ".#{ext}"
+          files = file.split ','
+          files.each_with_index do |f, i|
+            ext = File.extname(f)
+            f = f.gsub(ext, '')
+            ext.slice! 0
+
+            if i == 0
+              res.headers["Content-Type"] = "#{MimeMagic.by_extension(ext).to_s}; charset=utf-8"
             end
-          when 'js'
-            if js_assets.include? file + '.coffee'or js_head_assets.include? file + '.coffee'
-              res.write render "#{Assets.app.root}/app/assets/javascripts/#{file}.coffee"
-            else
-              res.write File.read "#{Assets.app.root}/app/assets/javascripts/#{file}.#{ext}"
-            end
-          else
-            res.write File.read "#{Assets.app.root}/app/assets/#{file}.#{ext}"
+
+            file_content += add_asset f, ext
           end
+
+          res.write file_content
+        else
+          res.headers["Content-Type"] = "#{MimeMagic.by_extension(ext).to_s}; charset=utf-8"
+
+          res.write add_asset file, ext
         end
       end
     end
